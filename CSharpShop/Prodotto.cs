dfb8b5518e3b44055d111e18aa4244799f187bdf
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-29 15:35:31 +0100

    codice random

--- CSharpShop/Prodotto.cs
+++ CSharpShop/Prodotto.cs
@@ -6,7 +6,81 @@ using System.Threading.Tasks;
 
 namespace CSharpShop
 {
-    internal class Prodotto
+    // Nel progetto CSharpShop , creare la classe Prodotto che gestisce i prodotti del vostro shop.
+    public class Prodotto
     {
+        // CARATTERISTICHE
+
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private float prezzo;
+        private int iva;
+
+        // COSTRUTTORI
+
+        public Prodotto()
+        {
+            this.codice = CodiceRandom();
+        }
+
+        public Prodotto(string nome, string descrizione, float prezzo, int iva)
+        {
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+        } 
+
+        // GETTERS
+
+        public int GetCodice()
+        {
+            return this.codice;
+        }
+        public string GetNome()
+        {
+            return this.nome;
+        }
+        public string GetDescrizione()
+        {
+            return this.descrizione;
+        }
+        public float GetPrezzo()
+        {
+            return this.prezzo;
+        }
+        public int GetIVA()
+        {
+            return this.iva;
+        }
+
+        // SETTERS
+
+        public void SetNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public void SetDescrizione(string descrizione)
+        {
+            this.descrizione = descrizione;
+        }
+        public void SetPrezo(float prezo)
+        {
+            this.prezzo = prezo;
+        }
+        public void SetIVA(int iva)
+        {
+            this.iva = iva;
+        }
+
+        //METODO
+
+        private int CodiceRandom()
+        {
+            Random random= new Random();
+            int codiceRandom = random.Next(99999999 + 1);
+            return codiceRandom;
+        }
     }
 }
--- CSharpShop/Program.cs
+++ CSharpShop/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CSharpShop;
+
+Prodotto Shop1= new Prodotto();
+
+Console.WriteLine(Shop1.GetCodice());
 
