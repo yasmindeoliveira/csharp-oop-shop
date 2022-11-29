1f793e824b308e25e76ec9e36181aeeb724ca0ae
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-29 17:06:35 +0100

    sistemata info prodotti

--- CSharpShop/Program.cs
+++ CSharpShop/Program.cs
@@ -2,12 +2,12 @@
 using CSharpShop;
 
 Prodotto Shop1 = new Prodotto("Diaspro Rosso", "Minerale con proprietà terapeutiche", 8.00f, 22);
-Prodotto Shop2 = new Prodotto("Calcite Arancione", "Minerale con proprietà terapeutiche", 8.00f, 22);
-Prodotto Shop3 = new Prodotto("Diaspro Giallo", "Minerale con proprietà terapeutiche", 8.00f,  22);
-Prodotto Shop4 = new Prodotto("Avventurina Verde", "Minerale con proprietà terapeutiche", 8.00f, 22);
+Prodotto Shop2 = new Prodotto("Calcite Arancione", "Minerale con proprietà terapeutiche", 7.00f, 22);
+Prodotto Shop3 = new Prodotto("Diaspro Giallo", "Minerale con proprietà terapeutiche", 9.00f,  22);
+Prodotto Shop4 = new Prodotto("Avventurina Verde", "Minerale con proprietà terapeutiche", 10.00f, 22);
 Prodotto Shop5 = new Prodotto("Amazonite", "Minerale con proprietà terapeutiche", 8.00f, 22);
-Prodotto Shop6 = new Prodotto("Sodalite", "Minerale con proprietà terapeutiche", 8.00f, 22);
-Prodotto Shop7 = new Prodotto("Ametista", "Minerale con proprietà terapeutiche", 8.00f, 22);
+Prodotto Shop6 = new Prodotto("Sodalite", "Minerale con proprietà terapeutiche", 4.00f, 22);
+Prodotto Shop7 = new Prodotto("Ametista", "Minerale con proprietà terapeutiche", 6.00f, 22);
 
 Shop1.Stampa();
 Shop2.Stampa();
